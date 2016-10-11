8f2e62526754cf9f48b826c37aa5dd14588a3199
Andrey Bogaevsky <andrey.bogaevsky@codeit.com.ua>
2016-10-11 12:07:16 +0300

    Fix: press of backspace button for "iOS" 8.0 - 8.2

--- Source/RKTagsView.m
+++ Source/RKTagsView.m
@@ -25,10 +25,40 @@ const CGFloat RKTagsViewAutomaticDimension = -0.0001;
 #pragma mark - RKInputTextField
 
 @implementation __RKInputTextField
+- (BOOL)keyboardInputShouldDelete:(UITextField *)textField {
+    
+    BOOL shouldDelete = YES;
+    
+    if ([UITextField instancesRespondToSelector:_cmd]) {
+        BOOL (*keyboardInputShouldDelete)(id, SEL, UITextField *) = (BOOL (*)(id, SEL, UITextField *))[UITextField instanceMethodForSelector:_cmd];
+        
+        if (keyboardInputShouldDelete) {
+            shouldDelete = keyboardInputShouldDelete(self, _cmd, textField);
+        }
+    }
+    
+    BOOL isIos8 = ([[[UIDevice currentDevice] systemVersion] intValue] == 8);
+    BOOL isLessThanIos8_3 = ([[[UIDevice currentDevice] systemVersion] floatValue] < 8.3f);
+    
+    if (![textField.text length] && isIos8 && isLessThanIos8_3) {
+        [self deleteBackward];
+    }
+    
+    return shouldDelete;
+}
+
 - (void)deleteBackward {
-  if ([self.tagsView shouldInputTextDeleteBackward]) {
-    [super deleteBackward];
-  }
+    BOOL shouldDismiss = [self.text length] == 0;
+    
+    if ([self.tagsView shouldInputTextDeleteBackward]) {
+        [super deleteBackward];
+    }
+    
+    if (shouldDismiss) {
+        if ([self.delegate respondsToSelector:@selector(textField:shouldChangeCharactersInRange:replacementString:)]) {
+            [self.delegate textField:self shouldChangeCharactersInRange:NSMakeRange(0, 0) replacementString:@""];
+        }
+    }
 }
 @end
 
